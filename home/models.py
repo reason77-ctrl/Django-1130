23aa77813370a760a9e9614ec1ab792222a58a0f
reason77-ctrl <reasonregmi25@hotmail.com>
2021-03-21 13:17:48 +0545

    Models edit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length= 300)
+    email = models.CharField(max_length= 300)
+    subject = models.TextField()
+    message = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -24,4 +25,16 @@ def blogsingle(request):
     return render(request, 'blog-single.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
     return render(request, 'contact.html')
